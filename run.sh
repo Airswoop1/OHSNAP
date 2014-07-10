fce661e0e41259d9a6b460b3acb56ce081b307e7
Kevin Miller <Airswoop1@gmail.com>
2014-07-10 21:14:07 +0000

    updated startup script

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@
 SC_DIR=/home/ec2-user/SC
 LOG_DIR=/home/ec2-user/logs
 
-nohup forever start $SC_DIR/server.js &
+nohup forever start -l $LOG_DIR/log.out -o $LOG_DIR/std_out.log -e $LOG_DIR/std_err.log --append $SC_DIR/server.js &
 
 echo 'Initialization of easyfoodstamps complete'
