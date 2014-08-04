7589afdd21cf925f36c12fc129592917292f66f4
Kevin Miller <Airswoop1@gmail.com>
2014-08-04 17:33:08 -0400

    updated server for prod

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.configure('sandbox', function(){
     app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
-app.configure('dev', function(){
+app.configure('prod', function(){
     app.use(express.errorHandler());
 
     app.use(function(req, res, next){
