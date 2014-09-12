48a800da07b441da44598657ceca5b3fb10b3228
Kevin Miller <Airswoop1@gmail.com>
2014-09-12 15:59:22 -0400

    adjusted for testing

--- public/js/Controllers/InterviewController.js
+++ public/js/Controllers/InterviewController.js
@@ -14,8 +14,8 @@ angular.module('formApp.interviewCtrl',['formApp.userDataFactory', 'formApp.apiF
 		$scope.show_esig_info = false;
 
 		$scope.interview_progress_status = 0;
-		//$scope.interview_steps = -1;
-		$scope.interview_steps = 5;
+		$scope.interview_steps = -1;
+		//$scope.interview_steps = 5;
 		$scope.user = userDataFactory.userData.user.formData; //? userDataFactory.userData.user.formData : {"household":1};
 		$scope.user.household_members = (typeof $scope.user.household_members!== 'undefined') ? $scope.user.household_members : {};
 		$scope.interviewCompleted = userDataFactory.userData.interviewProgress;
