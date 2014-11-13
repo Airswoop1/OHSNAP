1081a12034c631a5e35b4dc5304e6fac7a81602a
Ram Mehta <ram.mehta@gmail.com>
2014-11-13 16:51:27 -0500

    Fix issues with autoescaping in swig and change sandbox mongo to local.

--- config/sandbox.js
+++ config/sandbox.js
@@ -2,15 +2,14 @@
  * Created by airswoop1 on 6/27/14.
  */
 var fs = require('fs');
-var mongolab_pw = process.env.MONGOLAB_PW,
-    phaxio_key = process.env.PHAXIO_TEST_KEY,
+var phaxio_key = process.env.PHAXIO_TEST_KEY,
     phaxio_secret = process.env.PHAXIO_TEST_SECRET,
     AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY_ID,
     AWS_SECRET_KEY = process.env.AWS_SECRET_ACCESS_KEY;
 
 module.exports = {
     "db" : {
-        "mongodb" : "mongodb://sandbox:" + mongolab_pw + "@ds037447.mongolab.com:37447/ohsnap-sandbox"
+        "mongodb" : "mongodb://127.0.0.1:27017/ohsnap-sandbox"
     },
     "aws" : {
         "s3" : {
